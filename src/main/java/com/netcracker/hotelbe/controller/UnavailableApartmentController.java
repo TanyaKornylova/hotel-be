f77122965cdf1365122d179d78c5d6d760b57940
Shevchuk <ihshevchuk@gmail.com>
2019-12-04 18:32:54 +0200

    Configured loggingManager

--- src/main/java/com/netcracker/hotelbe/controller/UnavailableApartmentController.java
+++ src/main/java/com/netcracker/hotelbe/controller/UnavailableApartmentController.java
@@ -2,9 +2,6 @@ package com.netcracker.hotelbe.controller;
 
 import com.netcracker.hotelbe.entity.UnavailableApartment;
 import com.netcracker.hotelbe.service.UnavailableApartmentService;
-import com.netcracker.hotelbe.utils.CustomEntityLogMessage;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,45 +12,33 @@ import java.util.List;
 @RestController
 @RequestMapping("/unavailableApartments")
 public class UnavailableApartmentController {
-    private static Logger logger = LogManager.getLogger(UnavailableApartmentController.class);
-    private final static String ENTITY_NAME = UnavailableApartment.class.getSimpleName();
 
     @Autowired
     private UnavailableApartmentService unavailableApartmentService;
 
     @GetMapping
     public ResponseEntity<List<UnavailableApartment>> getAll() {
-        logger.info(String.format(CustomEntityLogMessage.REQUEST_FOR_GET_ALL_ENTITY, ENTITY_NAME));
-
         return new ResponseEntity<>(unavailableApartmentService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UnavailableApartment> getById(@PathVariable("id") final Long id) {
-        logger.info(String.format(CustomEntityLogMessage.REQUEST_FOR_GET_ENTITY_BY_ID, ENTITY_NAME, id));
-
-        return new ResponseEntity(unavailableApartmentService.findById(id), HttpStatus.OK);
+        return new ResponseEntity<>(unavailableApartmentService.findById(id), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<Long> create(@RequestBody UnavailableApartment unavailableApartment) {
-        logger.info(String.format(CustomEntityLogMessage.REQUEST_FOR_CREATE_ENTITY, ENTITY_NAME));
-
+    public ResponseEntity<UnavailableApartment> create(@RequestBody UnavailableApartment unavailableApartment) {
         return new ResponseEntity<>(unavailableApartmentService.save(unavailableApartment), HttpStatus.CREATED);
     }
 
 
     @PutMapping("/{id}")
-    public ResponseEntity<Long> update(@RequestBody UnavailableApartment unavailableApartment, @PathVariable("id") final Long id) {
-        logger.info(String.format(CustomEntityLogMessage.REQUEST_FOR_UPDATE_ENTITY_BY_ID, ENTITY_NAME, unavailableApartment.getId()));
-
+    public ResponseEntity<UnavailableApartment> update(@RequestBody UnavailableApartment unavailableApartment, @PathVariable("id") final Long id) {
         return new ResponseEntity<>(unavailableApartmentService.update(unavailableApartment, id), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity deleteById(@PathVariable("id") final Long id) {
-        logger.info(String.format(CustomEntityLogMessage.REQUEST_FOR_DELETE_ENTITY_BY_ID, ENTITY_NAME, id));
-
         unavailableApartmentService.deleteById(id);
 
         return new ResponseEntity(HttpStatus.OK);
